084a4d1cbf1a040a3a1d091d1497fad7fefc4f6e
marcher <Qing273344@163.com>
2019-06-24 13:56:56 +0800

    更新mq配置

--- src/main/java/xin/marcher/blog/consumers/ImgCopyConsumer.java
+++ src/main/java/xin/marcher/blog/consumers/ImgCopyConsumer.java
@@ -3,6 +3,7 @@ package xin.marcher.blog.consumers;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import xin.marcher.blog.biz.property.RabbitMqProperties;
 import xin.marcher.blog.service.BlogArticleContentService;
 
 /**
@@ -31,18 +32,4 @@ public class ImgCopyConsumer {
         blogArticleContentService.convertUrl(articleId);
     }
 
-    /**
-     * 监听队列
-     * 参数: 可直接写生产者发送的对象
-     */
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(value = "test_blog_content_queue", durable = "true"),
-            exchange = @Exchange(name = "marcher_blog_exchange"),
-            key = "test_blog_content_routekey"
-    ), containerFactory = "rabbitListenerContainerFactory"
-    )
-    @RabbitHandler
-    public void handlerTestMessage(Long articleId) {
-        blogArticleContentService.convertUrl(articleId);
-    }
 }
